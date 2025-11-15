caf997b1f130ec96c6e09388ae630e531d6bb7ef
baraka mukungu <bethelybaraka@gmail.com>
2025-11-15 22:03:37 +0300

    mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import mongoose from "mongoose"
 import 'dotenv/config'
 import { connectDB } from "./config/db.js"
 import foodRoutes from "./routes/foodRoutes.js"
@@ -7,11 +8,9 @@ import userRouter from "./routes/userRoutes.js"
 import cartRouter from "./routes/cartRoutes.js"
 import orderRouter from "./routes/orderRoutes.js"
 
-//app config
 const app = express()
 const port = 4000
 
-//middleware
 app.use(express.json())
 app.use(cors({
     origin: ["https://food-delivery-home.onrender.com"],
@@ -19,18 +18,22 @@ app.use(cors({
     credentials: true
 }));
 
-//endpoint for testing
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
-//endpoints
+
 app.use("/api/food",foodRoutes)
 app.use("/images",express.static('upload'))
 app.use("/api/data",userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
-//database connection
-connectDB() 
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err)); 
 
 //port listening
 app.listen(port,()=>{
