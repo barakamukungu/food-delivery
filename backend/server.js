d1772e2daf8ea14d73c3a81da175d6867c365b68
baraka mukungu <bethelybaraka@gmail.com>
2025-11-15 20:33:00 +0300

    server update

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = 4000
 
 //middleware
 app.use(express.json())
-app.use(cors()) 
+app.use(cors({
+    origin: ["https://food-delivery-home.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 //endpoint for testing
 app.get("/",(req,res)=>{
